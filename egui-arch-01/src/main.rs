9e7e7ded21352be95377e88e659f5ffd2476b253
benracine <benracinedev@gmail.com>
2026-04-22 20:56:29 -0500

    .

--- egui-arch-01/src/main.rs
+++ egui-arch-01/src/main.rs
@@ -2,7 +2,7 @@ use egui_arch_01::App;
 
 fn main() -> eframe::Result<()> {
     eframe::run_native(
-        "Egui Clean Architecture (Arming Enforced)",
+        "Egui Clean Architecture",
         eframe::NativeOptions::default(),
         Box::new(|_cc| Ok(Box::new(App::default()))),
     )
